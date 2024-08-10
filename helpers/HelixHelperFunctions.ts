ae3b95e47ec2324a96706cca1f78c8742ad35b30
orhunakkan <orhunakkan@outlook.com>
2024-08-09 23:57:06 -0500

    Remove Helix related login and bucket creation tests
    Deleted test files related to Helix login and bucket creation, as well as associated helper functions and locators. These removals indicate a possible deprecation or replacement of these features in the testing framework.

--- helpers/HelixHelperFunctions.ts
+++
@@ -1,6 +0,0 @@
-export function generateUniqueString(maxLength: number = 30): string {
-    const time = Date.now().toString(36).substring(0, 11);
-    const randomLength = maxLength - time.length - 1;
-    const random = Math.random().toString(36).substring(2, 2 + randomLength);
-    return `${time}_${random}`.substring(0, maxLength);
-}
--- locators/HelixLoginPageLocators.ts
+++
@@ -1,5 +0,0 @@
-import { Page } from '@playwright/test';
-
-export const HelixLoginPageLocators = {
-    getLoginButton: (page: Page) => page.getByRole("button", {name: "Login"}),
-};
