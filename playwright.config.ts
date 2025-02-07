bf09a5640b4e3249d75139a3b00b0cb7508825fc
Orhun Akkan <orhun.akkan@aspirion.com>
2025-02-07 11:04:59 -0600

    Enhance Playwright testing workflow with Allure reporting and update dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,34 +1,35 @@
-import {defineConfig, devices} from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
+import { AllureReporter } from 'allure-playwright';
 
 export default defineConfig({
-    testDir: './tests',
-    snapshotPathTemplate: '{testDir}/{testFileDir}/snapshots/{testFileName}-{projectName}{ext}',
-    timeout: 30000,
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 3 : 0,
-    workers: process.env.CI ? 1 : undefined,
-    reporter: 'html',
-    use: {
-        baseURL: process.env.ENV,
-        trace: 'on-first-retry',
-        ignoreHTTPSErrors: true,
+  testDir: './tests',
+  snapshotPathTemplate: '{testDir}/{testFileDir}/snapshots/{testFileName}-{projectName}{ext}',
+  timeout: 30000,
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 3 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [ ['line'], ['allure-playwright'] ],
+  use: {
+    baseURL: process.env.ENV,
+    trace: 'on-first-retry',
+    ignoreHTTPSErrors: true,
+  },
+  projects: [
+    {
+      name: 'Desktop Chromium',
+      testDir: './tests',
+      use: { ...devices['Desktop Chrome'] },
     },
-    projects: [
-        {
-            name: 'Desktop Chromium',
-            testDir: './tests',
-            use: {...devices['Desktop Chrome']},
-        },
-        // {
-        //     name: 'Desktop Firefox',
-        //     testDir: './tests',
-        //     use: {...devices['Desktop Firefox']},
-        // },
-        // {
-        //     name: 'Desktop Webkit',
-        //     testDir: './tests',
-        //     use: {...devices['Desktop Safari']},
-        // },
-    ],
+    // {
+    //     name: 'Desktop Firefox',
+    //     testDir: './tests',
+    //     use: {...devices['Desktop Firefox']},
+    // },
+    // {
+    //     name: 'Desktop Webkit',
+    //     testDir: './tests',
+    //     use: {...devices['Desktop Safari']},
+    // },
+  ],
 });
