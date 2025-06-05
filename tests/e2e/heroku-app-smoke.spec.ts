54cab7cbc40e73538212d14ba816993d3434c62e
orhunakkan <orhunakkan@outlook.com>
2025-06-05 14:40:54 -0500

    Remove unnecessary tag from smoke suite description in Heroku app test

--- tests/e2e/heroku-app-smoke.spec.ts
+++ tests/e2e/heroku-app-smoke.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "playwright/test";
 
-test.describe("Heroku App - Smoke Suite @smoke", () => {
+test.describe("Heroku App - Smoke Suite", () => {
 
   test.beforeEach(async ({ page }) => {
     await page.goto("https://the-internet.herokuapp.com/");
