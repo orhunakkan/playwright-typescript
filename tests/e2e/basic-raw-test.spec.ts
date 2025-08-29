f3486321f8e59d4670c1593a4c68696ded00178a
Orhun Akkan <orhunakkan@outlook.com>
2025-08-29 13:52:36 -0500

    feat: update test directory structure and add initial homepage validation test

---
+++ tests/e2e/basic-raw-test.spec.ts
@@ -0,0 +1,8 @@
+import {expect, test} from 'playwright/test';
+
+test.describe('Homepage URL validation', () => {
+  test('should go to homepage and verify its URL', async ({page}) => {
+    await page.goto('https://bonigarcia.dev/selenium-webdriver-java/index.html');
+    expect(await page.title()).toEqual('Hands-On Selenium WebDriver with Java');
+  })
+})
