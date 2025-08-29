42d618e26a81983d031dc7dca321b40e58675ed7
Orhun Akkan <orhunakkan@outlook.com>
2025-08-29 13:59:02 -0500

    fix: import issue fixed

--- tests/e2e/basic-raw-test.spec.ts
+++ tests/e2e/basic-raw-test.spec.ts
@@ -1,4 +1,4 @@
-import {expect, test} from 'playwright/test';
+import {expect, test} from '@playwright/test';
 
 test.describe('Homepage URL validation', () => {
   test('should go to homepage and verify its URL', async ({page}) => {
