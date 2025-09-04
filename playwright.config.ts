2c633d688be2bfebb48ddef6ee60cfd9eaf5d100
orhunakkan <orhunakkan@outlook.com>
2025-09-04 00:54:18 -0500

    feat: update test directory structure and add restful-booker API tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests/e2e',
+  testDir: './tests',
   timeout: 30000,
   fullyParallel: true,
   retries: process.env.CI ? 1 : 0,
---
+++ tests/api/restful-booker-api-flow.spec.js
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test';
+
+
+let sessionToken;
+const baseURL = 'https://restful-booker.herokuapp.com';
+
+test.describe('restful-booker APIs', () => {
+  test('should create token', async ({request}) => {
+    const response = await request.post(
+      `${baseURL}/auth`,
+      {
+        username : "admin",
+        password : "password123"
+      });
+    expect(response.status()).toEqual(200);
+  });
+
+  test('should get Booking Ids', async ({request}) => {
+    const response = await request.get(`${baseURL}/booking`);
+    expect(response.status()).toEqual(200);
+  });
+})
