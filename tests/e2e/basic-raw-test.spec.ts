704722c7c7a68051a7b8a563d712691c4d5b63af
orhunakkan <orhunakkan@outlook.com>
2025-10-21 23:32:00 -0500

    feat: add visual regression test and refactor homepage URL validation tests

--- tests/e2e/basic-raw-test.spec.ts
+++ tests/e2e/basic-raw-test.spec.ts
@@ -1,8 +1,15 @@
 import {expect, test} from '@playwright/test';
 
 test.describe('Homepage URL validation', () => {
-  test('should go to homepage and verify its URL', async ({page}) => {
+  test.beforeEach('should go to homepage', async ({page}) => {
     await page.goto('https://bonigarcia.dev/selenium-webdriver-java/index.html');
+  })
+
+  test('should verify its URL', async ({page}) => {
     expect(await page.title()).toEqual('Hands-On Selenium WebDriver with Java');
+  })
+
+  test('visual regression test', async ({page}) => {
+    await expect(page).toHaveScreenshot({fullPage: true});
   })
 })
