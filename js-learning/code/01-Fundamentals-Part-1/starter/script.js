62a595524d30a2856a3a6f41c6d69142f8706a28
orhunakkan <orhunakkan@outlook.com>
2025-11-16 08:02:52 -0600

    feat: expand JavaScript fundamentals with additional primitive data types examples

--- js-learning/code/01-Fundamentals-Part-1/starter/script.js
+++ js-learning/code/01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,32 @@
 let js = 'Consistency is key!';
 console.log(40 + 10);
+
+let firstName = 'ORHUN';
+console.log(firstName);
+
+// -----Primitive Data Types-----
+
+let age = 35;
+let myName  = 'ORHUN';
+let isGoodAtJavascript = false;
+let thisIsUndefined;
+// null
+// symbol -> ES2015
+// BigInt -> ES2020
+
+console.log(typeof age);
+console.log(typeof myName);
+console.log(typeof isGoodAtJavascript);
+
+console.log(isGoodAtJavascript);
+isGoodAtJavascript = 'YES';
+console.log(isGoodAtJavascript);
+
+console.log(thisIsUndefined);
+console.log(typeof thisIsUndefined);
+
+thisIsUndefined = 'Not anymore';
+console.log(thisIsUndefined);
+console.log(typeof thisIsUndefined);
+
+console.log(typeof null);
