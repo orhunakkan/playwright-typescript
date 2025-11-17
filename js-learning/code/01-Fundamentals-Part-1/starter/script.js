874ca4280e84f2e37b2907f9a5402a789066c6d5
orhunakkan <orhunakkan@outlook.com>
2025-11-16 22:36:25 -0600

    feat: add comprehensive examples of variable declaration, operators, and template literals in JavaScript

--- js-learning/code/01-Fundamentals-Part-1/starter/script.js
+++ js-learning/code/01-Fundamentals-Part-1/starter/script.js
@@ -4,17 +4,17 @@ console.log(40 + 10);
 let firstName = 'ORHUN';
 console.log(firstName);
 
-// -----Primitive Data Types-----
+// ----- Primitive Data Types -----
 
-let age = 35;
-let myName  = 'ORHUN';
+let xage = 35;
+let myName = 'ORHUN';
 let isGoodAtJavascript = false;
 let thisIsUndefined;
 // null
 // symbol -> ES2015
 // BigInt -> ES2020
 
-console.log(typeof age);
+console.log(typeof xage);
 console.log(typeof myName);
 console.log(typeof isGoodAtJavascript);
 
@@ -30,3 +30,41 @@ console.log(thisIsUndefined);
 console.log(typeof thisIsUndefined);
 
 console.log(typeof null);
+
+// ----- let, const, var -----
+
+let age = 35;
+const birthYear = 1990;
+// const job; -> this is not allowed
+
+var job = 'programmer';
+job = 'teacher';
+console.log(job);
+
+// Math Operators
+const now = 2050;
+const ageOrhun = now - 1990;
+const ageRabia = now - 1992;
+console.log(ageOrhun, ageRabia);
+console.log(ageOrhun * 2, ageRabia / 2);
+
+const myFirstName = 'ORHUN';
+const myLastName = 'AKKAN';
+console.log(myFirstName + ' ' + myLastName);
+
+// Assignment Operators
+let x = 15;
+x += 10;
+x *= 5;
+x++;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageOrhun > ageRabia); // >, <, >=, <=
+
+// Operator Precedence is followed in calculations.
+
+// Template Literals
+const orhun = `My name is ${myFirstName} and my last name is ${myLastName}`;
+console.log(orhun);
